564c134ae1be848918da576ac9c9a30c08fff589
Jana Uhrich <janamu@uw.edu>
2019-08-28 14:58:54 -0700

    put route to loops through given updates won't wipe out scores

--- api/routes/assignments.js
+++ api/routes/assignments.js
@@ -2,34 +2,41 @@ const router = require('express').Router({ mergeParams: true })
 const User = require('../models/user')
 const { isLoggedIn, isSameUser } = require('../middleware/auth')
 
+//starting route http://localhost:5000/api/users/:userId/assignments
+
+//create an assignment, middleware checks for authentication & authorization
 router.post('/', isLoggedIn, isSameUser, async (req, res, next) => {
   const status = 201
-
+  // lookup the user with the userID 
   const { userId } = req.params
   const query = { _id: userId }
   const user = await User.findOne(query)
   
+  //push the json body to the assignments array creating a new assignment and save it
   user.assignments.push(req.body)
   await user.save()
   
+  //respond to the request with the last assignment in the array
   const assignment = user.assignments[user.assignments.length - 1]
+
   res.status(status).json({ status, response: assignment })
 })
 
+//update an assignment, middleware checks for authentication & authorization
 router.put('/:assignmentId', isLoggedIn, isSameUser, async (req, res, next) => {
   const status = 200
 
+  //get userId and assignmentId from the params, use them to look up the user and the assignment
   const { assignmentId, userId } = req.params
   const query = { _id: userId }
   const user = await User.findOne(query)
   const assignment = user.assignments.id(assignmentId)
-  
-  const { title, project_description, project_link, score, base } = req.body
-  assignment.title = title
-  assignment.project_description = project_description
-  assignment.project_link = project_link
-  assignment.score = score
-  assignment.base = base
+
+//loop through each object and update if it changed
+  for (let update in req.body) {
+    assignment[update] = req.body[update];
+  }
+
   await user.save()
   
   res.status(status).json({ status, response: assignment })
